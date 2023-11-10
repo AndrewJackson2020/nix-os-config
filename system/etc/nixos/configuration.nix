a15fb33f623426123142ba5c5bc3517ef815f2d8
Andrew Jackson <andrewjackson947@gmail.com>
2023-11-09 21:33:19 -0600

    REvert.

--- system/etc/nixos/configuration.nix
+++ system/etc/nixos/configuration.nix
@@ -58,7 +58,7 @@
   services.xserver.enable = true;
   services.xserver.autorun = false;
   services.xserver.layout = "us";
-  services.xserver.desktopManager.defaultSession = "none+i3";
+  services.xserver.desktopManager.default = "none+i3";
   services.xserver.desktopManager.xterm.enable = false;
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.windowManager.i3.enable = true;
