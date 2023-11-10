72dca276d3f214a7b177bada3e75eb7c86fea386
Andrew Jackson <andrewjackson947@gmail.com>
2023-11-09 20:39:02 -0600

    Attempt to include i3

--- system/etc/nixos/configuration.nix
+++ system/etc/nixos/configuration.nix
@@ -44,18 +44,38 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
+  # services.xserver = {
+    # layout = "us";
+    # xkbVariant = "";
+  # };
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
+    enable = true;
 
+    desktopManager = {
+      xterm.enable = false;
+    };
+   
+    displayManager = {
+        defaultSession = "none+i3";
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu #application launcher most people use
+        i3status # gives you the default i3 status bar
+        i3lock #default i3 screen locker
+        i3blocks #if you are planning on using i3blocks over i3status
+     ];
+    };
+  };
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
@@ -129,6 +149,7 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
+  environment..pathsToLink = [ "/libexec" ];
   environment.systemPackages = with pkgs; [
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      git
