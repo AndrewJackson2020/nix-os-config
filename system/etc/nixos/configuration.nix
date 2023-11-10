9da631d82d95940d067a46a8b79f5757e9074dc1
Andrew Jackson <andrewjackson947@gmail.com>
2023-11-09 21:41:07 -0600

    Fix

--- system/etc/nixos/configuration.nix
+++ system/etc/nixos/configuration.nix
@@ -151,7 +151,6 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.pathsToLink = [ "/libexec" ];
   environment.systemPackages = with pkgs; [
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
      git
