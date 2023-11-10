dc3d9e42851d92b5c0eb5323c0965a8304341723
Andrew Jackson <andrewjackson947@gmail.com>
2023-11-10 07:22:15 -0600

    Attempted fix.

--- system/etc/nixos/configuration.nix
+++ system/etc/nixos/configuration.nix
@@ -58,7 +58,7 @@
   environment.pathsToLink = [ "/libexec" ];
   services.xserver = {
     enable = true;
-    # modules = [ pkgs.xorg.xf86videofbdev ];
+    modules = [ pkgs.xorg.xf86videofbdev ];
     videoDrivers = [ "hyperv_fb" ];
     desktopManager = {
       xterm.enable = false;
