fb9ed4b53adc78df75b8c869e635e4ab47f077e8
CommanderKeynes <andrewjackson947@gmail.com>
2024-03-18 20:15:19 -0500

    Incorporated home manager into nixos

--- flake.nix
+++ flake.nix
@@ -4,9 +4,15 @@
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    # Home manager
+    home-manager= {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     # Please replace my-nixos with your hostname
     nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -14,7 +20,14 @@
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
-	./home.nix
+        home-manager.nixosModules.home-manager{
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.andrew = import ./home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+        }
       ];
     };
   };
