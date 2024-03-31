812c3ca552401999e22606e635a3e1fb2c19ce57
CommanderKeynes <andrewjackson947@gmail.com>
2024-03-31 14:52:32 -0500

    Chaanged

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      ./hardware_configuration/laptop.nix
       # <home-manager/nixos>
     ];
 
