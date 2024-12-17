9c69ccc31d401fc121e8b6d8042fc5b2e34a0f3f
CommanderKeynes <andrewjackson947@gmail.coma>
2024-12-16 21:35:57 -0600

    Format

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     # Home manager
-    home-manager= {
+    home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
